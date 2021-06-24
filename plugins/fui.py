201d1f8bce4b7e9ba0677bdfc7f8d4d2a9a5be86
namahee <85374690+namahee@users.noreply.github.com>
2021-06-24 12:44:31 -0300

    Add files via upload

---
+++ plugins/fui.py
@@ -0,0 +1,32 @@
+""" NoteX/Samuca @samuca78 Snooze """
+
+import asyncio
+
+from userge import Message, userge
+
+
+@userge.on_cmd(
+    "fui$",
+    about={
+        "header": "executa .afk",
+        "flags": {
+            "-s": "Sleeping",
+            "-b": "Busy",
+            "-w": "Watching",
+        },
+    },
+    trigger="",
+    allow_via_bot=False,
+)
+async def reset_(message: Message):
+    """fuck"""
+    fuck = "!fui Zzz... <https://telegra.ph/file/5f5ef5dde5e811ab753b5.mp4>"
+    await message.try_to_edit(fuck, del_in=1)
+
+
+async def check_and_send(message: Message, *args, **kwargs):
+    replied = message.reply_to_message
+    if replied:
+        await asyncio.gather(message.delete(), replied.reply(*args, **kwargs))
+    else:
+        await message.edit(*args, **kwargs)
