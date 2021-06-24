c7872113855ac64e96d687c307a39a3a4e229c71
namahee <85374690+namahee@users.noreply.github.com>
2021-06-24 14:36:51 -0300

    update fui.py

--- plugins/fui.py
+++ plugins/fui.py
@@ -20,7 +20,7 @@ from userge import Message, userge
 )
 async def reset_(message: Message):
     """fuck"""
-    fuck = "!fui Zzz... <https://telegra.ph/file/5f5ef5dde5e811ab753b5.mp4>"
+    fuck = "!afk Zzz... <https://telegra.ph/file/5f5ef5dde5e811ab753b5.mp4>"
     await message.try_to_edit(fuck, del_in=1)
 
 
