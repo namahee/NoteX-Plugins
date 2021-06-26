6c0d560f6a27917c4f02dbb21f5acff8382e89a6
namahee <85374690+namahee@users.noreply.github.com>
2021-06-26 17:35:03 -0300

    update afk.py

--- plugins/afk.py
+++ plugins/afk.py
@@ -6,7 +6,7 @@ from random import randint
 from re import compile as comp_regex
 
 
-from userge.plugins.bot.alive import Bot_alive
+from userge.plugins.bot.alive import Bot_Alive
 
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultAnimation
@@ -130,7 +130,7 @@ async def handle_afk_incomming(message: Message) -> None:
                         chat_id,
                         animation=match.group(0),
                         caption=_afk_.out_str(),
-                        reply_markup=Bot_alive.alive_buttons(),
+                        reply_markup=Bot_Alive.alive_buttons(),
                     )
                 elif type_ == "url_image":
                     await client.send_photo(
