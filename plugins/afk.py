800ec1319b5b19c27b492582eab41efa13761053
namahee <85374690+namahee@users.noreply.github.com>
2021-06-27 20:53:42 -0300

    update afk.py

--- plugins/afk.py
+++ plugins/afk.py
@@ -120,12 +120,12 @@ async def handle_afk_incomming(message: Message) -> None:
             if match:
                 type_, media_ = await _afk_.check_media_link(match.group(0))
                 if type_ == "url_image":
-                    await send_inline_afk_(message)
+                    await _afk_.send_inline_afk_(message)
                 elif type_ == "url_gif":
-                    await send_inline_afk(message)
+                    await _afk_.send_inline_afk(message)
             else:
                 coro_list.append(
-                    await _send_inline_afk(message)
+                    await _afk_._send_inline_afk(message)
                 )
                 # coro_list.append(
                     # message.reply(_afk_._out_str())
@@ -139,12 +139,12 @@ async def handle_afk_incomming(message: Message) -> None:
         if match:
             type_, media_ = await _afk_.check_media_link(match.group(0))
             if type_ == "url_image":
-                await send_inline_afk_(message)
+                await _afk_.send_inline_afk_(message)
             elif type_ == "url_gif":
-                await send_inline_afk(message)
+                await _afk_.send_inline_afk(message)
         else:
             coro_list.append(
-                await _send_inline_afk(message)
+                await _afk_._send_inline_afk(message)
             )
             # coro_list.append(
                 # message.reply(_afk_._out_str())
