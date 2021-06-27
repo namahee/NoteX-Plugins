eb01840654ef3c6da85b35a884f4cc5ce557f593
namahee <85374690+namahee@users.noreply.github.com>
2021-06-27 01:28:07 -0300

    update afk.py

--- plugins/afk.py
+++ plugins/afk.py
@@ -6,6 +6,7 @@ from random import randint
 from re import compile as comp_regex
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors import BadRequest, FloodWait, Forbidden, MediaEmpty
 
 from userge import Config, Message, filters, get_collection, userge
 from userge.utils import time_formatter
