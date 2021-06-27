b78cc9f37ad7cf3551f779a806aa9e978707378b
namahee <85374690+namahee@users.noreply.github.com>
2021-06-27 15:59:59 -0300

    update afk.py

--- plugins/afk.py
+++ plugins/afk.py
@@ -121,6 +121,13 @@ async def _send_inline_afk(message: Message):
         chat_id=message.chat.id, query_id=_x.query_id, result_id=_x.results[0].id
     )
     
+async def send_inline_afk_(message: Message):
+    _bot_ = await userge.bot.get_me()
+    _x_ = await userge.get_inline_bot_results(_bot_.username, "test")
+    await userge.send_inline_bot_result(
+        chat_id=message.chat.id, query_id=_x_.query_id, result_id=_x_.results[0].id
+    )
+    
 
 async def handle_afk_incomming(message: Message) -> None:
     """handle incomming messages when you afk"""
@@ -138,12 +145,12 @@ async def handle_afk_incomming(message: Message) -> None:
         if not (USERS[user_id][0] + USERS[user_id][1]) % randint(2, 4):
             match = _TELE_REGEX.search(REASON)
             if match:
-                # await send_inline_afk(message)
-                type_, media_ = await _afk_.check_media_link(match.group(0))
-                if type_ == "url_gif":
-                    await send_inline_afk(message)
-                elif type_ == "url_image":
-                    await send_inline_afk_(message)
+                await send_inline_afk(message)
+                # type_, media_ = await _afk_.check_media_link(match.group(0))
+                # if type_ == "url_gif":
+                    # await send_inline_afk(message)
+                # elif type_ == "url_image":
+                    # await send_inline_afk_(message)
                     # NOT
                     # r = REASON.split(" | ", maxsplit=1)
                     # STATUS = r[0]
