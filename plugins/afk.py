9995bb365de9e8b2ae8f66518895d7c13d0e3869
namahee <85374690+namahee@users.noreply.github.com>
2021-06-27 21:52:38 -0300

    update afk.py

--- plugins/afk.py
+++ plugins/afk.py
@@ -129,12 +129,12 @@ async def handle_afk_incomming(message: Message) -> None:
         if not (USERS[user_id][0] + USERS[user_id][1]) % randint(2, 4):
             match = _TELE_REGEX.search(REASON)
             if match:
-                await _send_inline_afk_(message)
-                # type_, media_ = await _afk_.check_media_link(match.group(0))
-                # if type_ == "url_image":
-                    # await send_inline_afk_(message)
-                # elif type_ == "url_gif":
-                    # await send_inline_afk(message)
+                # await _send_inline_afk_(message)
+                type_, media_ = await _afk_.check_media_link(match.group(0))
+                if type_ == "url_image":
+                    await send_inline_afk_(message)
+                elif type_ == "url_gif":
+                    await send_inline_afk(message)
             else:
                 coro_list.append(
                     await _send_inline_afk(message)
@@ -150,11 +150,11 @@ async def handle_afk_incomming(message: Message) -> None:
         match = _TELE_REGEX.search(REASON)
         if match:
             await _send_inline_afk_(message)
-            # type_, media_ = await _afk_.check_media_link(match.group(0))
-            # if type_ == "url_image":
-                # await send_inline_afk_(message)
-            # elif type_ == "url_gif":
-                # await send_inline_afk(message)
+            type_, media_ = await _afk_.check_media_link(match.group(0))
+            if type_ == "url_image":
+                await send_inline_afk_(message)
+            elif type_ == "url_gif":
+                await send_inline_afk(message)
         else:
             coro_list.append(
                 await _send_inline_afk(message)
