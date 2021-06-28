06fa8b3095f7fe1f9fe35057bdbd31936cd083f5
namahee <85374690+namahee@users.noreply.github.com>
2021-06-28 00:26:53 -0300

    update afk.py

--- plugins/afk.py
+++ plugins/afk.py
@@ -120,7 +120,7 @@ async def handle_afk_incomming(message: Message) -> None:
         if not (USERS[user_id][0] + USERS[user_id][1]) % randint(2, 4):
             match = _TELE_REGEX.search(REASON)
             if match:
-                type_, media_ = await _afk_.check_media_link(match.group(0))
+                type_, media_ = await _afk.check_media_link(match.group(0))
                 if type_ == "url_image":
                     await send_inline_afk_(message)
                 elif type_ == "url_gif":
