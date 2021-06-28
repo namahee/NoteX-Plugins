ddaaf3170d21f573889306d898aa923a26760048
namahee <85374690+namahee@users.noreply.github.com>
2021-06-27 22:14:57 -0300

    update afk.py

--- plugins/afk.py
+++ plugins/afk.py
@@ -108,7 +108,7 @@ async def active_afk(message: Message) -> None:
 
 async def _send_inline_afk_(message: Message):
     _bot = await userge.bot.get_me()
-     _x = await userge.get_inline_bot_results(_bot.username, "cafe")
+    _x = await userge.get_inline_bot_results(_bot.username, "cafe")
     await userge.send_inline_bot_result(
         chat_id=message.chat.id, query_id=_x.query_id, result_id=_x.results[0].id
     )
