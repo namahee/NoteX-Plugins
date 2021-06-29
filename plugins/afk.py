693989123b789fad63c80d0c45bc02e1e7dbc257
namahee <85374690+namahee@users.noreply.github.com>
2021-06-28 21:04:09 -0300

    update afk.py

--- plugins/afk.py
+++ plugins/afk.py
@@ -248,7 +248,7 @@ async def status_afk_(_, c_q: CallbackQuery):
         # await c_q.answer(
             # f"Last Seen: {_afk_time_}\nDev: @NoteZV",
             # show_alert=True,
-        )
+        # )
     return status_afk_
         
         
