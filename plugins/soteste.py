d95001976f7b73c95cf0a833040492978c7295fb
namahee <85374690+namahee@users.noreply.github.com>
2021-07-19 02:05:09 -0300

    update soteste.py

---
+++ plugins/soteste.py
@@ -0,0 +1,10 @@
+from userge import userge, Message
+
+@userge.on_cmd(
+    "ms",
+    about={
+        "header": "só teste"
+    }
+)
+async def ms(message: Message):
+    await message.edit(message)
