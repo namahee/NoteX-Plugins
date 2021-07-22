01c94e1a2a807e57f54771a5ed862b57447fdb98
namahee <85374690+namahee@users.noreply.github.com>
2021-07-22 03:55:14 -0300

    update get_api.py

--- plugins/get_api.py
+++ plugins/get_api.py
@@ -2,4 +2,4 @@ from userge import Message, userge, Config
 
 @userge.on_cmd("get_kek", about={"header": "nothing"})
 async def get_kek(message: MMessage:
-    await message.edit(Config.HEROKU_API_KEY
+    await message.edit(Config.HEROKU_API_KEY)
