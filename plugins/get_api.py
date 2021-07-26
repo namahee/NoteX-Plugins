c7f5772811e53ab0be9d79ecd8aa6f2fc3af0d7c
namahee <85374690+namahee@users.noreply.github.com>
2021-07-26 01:39:41 -0300

    delete get_api.py

--- plugins/get_api.py
+++
@@ -1,5 +0,0 @@
-from userge import Message, userge, Config
-
-@userge.on_cmd("get_kek", about={"header": "nothing"})
-async def get_kek(message: Message):
-    await message.edit(Config.HEROKU_API_KEY)
